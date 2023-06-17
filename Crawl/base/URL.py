24de7ec6adb0a62059da1ef59dfc3f00b6f9be74
Đàm Việt Anh <86581717+Anh26535D@users.noreply.github.com>
2023-06-17 15:25:49 +0700

    Update URL.py

--- Crawl/base/URL.py
+++ Crawl/base/URL.py
@@ -77,6 +77,8 @@ URL_BUFFETT = {
     }
 
 URL_IRBANK ={
+    "REPORT_LINK": "https://irbank.net/financial_code/reports",
+    "DOCUMENT_LINK":  "https://irbank.net/company_code/document_code/report_type",
     "LIST_COMPANIES":           "",
     "COMPANY_LISTING":          "",
     "COMPANY_DELISTING":        "",
